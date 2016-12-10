a0e72dda4cfdc7dd51012a9228218a118ed5b394
Michael <mike.r.jenkins.jr@gmail.com>
2016-12-09 23:00:14 -0500

    added assets and csv

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,14 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Michael Jenkins on 12/9/16.
+//  Copyright © 2016 Blades. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+
+
+}//End Pokemon Class
